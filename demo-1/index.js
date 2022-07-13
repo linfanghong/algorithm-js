28a9af623b6b73b4b204a54240ee32538803d629
linfanghong <linfanghong1997@163.com>
2022-07-13 20:09:55 +0800

    leetcode 简单 1题

--- demo-1/index.js
+++ demo-1/index.js
@@ -12,3 +12,37 @@
         }
     }
 };
+// 这是暴力解法 复杂度为O(n^2)
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    const store = {}
+    for(let i = 0; i < nums.length; i++) {
+        if(store[nums[i]] !== undefined){
+            return [ store[nums[i]], i]
+        } else {
+            store[target - nums[i]] = i
+        }
+    }
+};
+// 这是对配对的另一个数字进行记录 复杂度为O(1)
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    const store2 = {}
+    for(let i = 0; i < nums.length; i++) {
+        if(store2[target - nums[i]] !== undefined){
+            return [ store2[target - nums[i]], i]
+        } else {
+            store2[nums[i]] = i
+        }
+    }
+};
