f61a296895b8fa3471e1995316d1b7c56ae6d83d
linfanghong <linfanghong1997@163.com>
2022-08-01 22:23:57 +0800

    create: demo-21

---
+++ demo-21/index.js
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(list1, list2) {
+  const result = new ListNode(-200)
+  let pre = result
+  while(list1!==null && list2!==null) {
+      if(list1.val < list2.val){
+          pre.next = list1
+          list1 = list1.next
+      }
+      else {
+          pre.next = list2
+          list2 = list2.next
+      }
+      pre = pre.next
+  }
+  if(list1 === null) {
+      pre.next = list2
+  }
+  else if(list2 === null) {
+      pre.next = list1
+  }
+  return result.next
+};
+
+// 递归的方式
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(list1, list2) {
+  if(list1 === null) {
+      return list2
+  }
+  else if(list2 === null) {
+      return list1
+  }
+  if(list1.val < list2.val) {
+      list1.next = mergeTwoLists(list1.next, list2)
+      return list1
+  }
+  else {
+      list2.next = mergeTwoLists(list1, list2.next)
+      return list2
+  }
+};
