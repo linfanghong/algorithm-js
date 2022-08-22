6f08d7b8aeae8ccef68d9d26f7ab789ad4f7177f
linfanghong <linfanghong1997@163.com>
2022-08-22 19:55:54 +0800

    增加第五题题解

---
+++ leetcode/005寻找字符串中的最长回文子串.js
@@ -0,0 +1,28 @@
+// 题目链接：https://leetcode.cn/problems/longest-palindromic-substring/
+
+// 暴力解法
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var longestPalindrome = function(s) {
+  result = []
+  for(let i=0; i<s.length; i++){
+      for(let j=s.length; j>i; j--){
+          if(s.slice(i,j) === s.slice(i,j).split('').reverse().join('')){
+              result.push(s.slice(i,j));
+              break;
+          }
+      }
+  }
+  let maxString = ""
+  for(let item of result){
+      if(item.length>maxString.length){
+          maxString = item;
+      }
+  }
+  return maxString;
+};
+// 运行会超时
+
+// 双指针
